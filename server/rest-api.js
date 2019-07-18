c6d9ab166510fa03172368ff7a0c2a03f41f7861
David Robbins <robbins.david@gmail.com>
2019-07-18 12:39:20 -0500

    API tokens now required for all REST API usage

--- server/rest-api.js
+++ server/rest-api.js
@@ -6,24 +6,52 @@ const API_PREFIX = "/api/1.0"
 // });
 
 Meteor.methods({
-    'test.getUser'(){
-        return Meteor.userId();
+    'apiToken.confirm'(token){
+        console.log(`Validating token ${token}`)
+        if (apiTokens.findOne({token})) {
+            return true;
+        } else {
+            return false;
+        }
     }
 });
 
-WebApp.connectHandlers.use(API_PREFIX+'/hello', async (req, res, next) => {
-    let json = {}
-    try{
-        json = await Meteor.call("test.getUser");
-        console.log(json);
+WebApp.connectHandlers.use(async (req, res, next) => {
+    const token = req.query.token
+    let confirmedToken = false;
+
+    try {
+        confirmedToken = await Meteor.call("apiToken.confirm", token)
     } catch(e) {
-        console.error('/hello - err:\n',e);
+        console.error('API Token Error - err:\n',e);
     }
 
+    if (confirmedToken) {
+        next();
+    } else {
+        res.writeHead(401)
+        res.end(`Token missing or unauthorized.`)
+    }
+})
 
-
-  // do something with json/body
-
+WebApp.connectHandlers.use(API_PREFIX+'/hello', (req, res, next) => {
   res.writeHead(200);
-  res.end(`Hello world from: ${Meteor.release}`);
+  res.end(`Hello authorized user from: ${Meteor.release}`);
+})
+
+WebApp.connectHandlers.use(API_PREFIX+'/patients', (req, res, next) => {
+    let json = {}
+    // try{
+    //     json = await Meteor.call("test.getUser");
+    //     console.log(json);
+    // } catch(e) {
+    //     console.error('/hello - err:\n',e);
+    // }
+    // do something with json/body
+
+    console.log(JSON.stringify(req.query, 2));
+    console.log(Object.keys(req));
+
+    res.writeHead(200);
+    res.end(`Hello Patient`);
 })
