e48c7b259b7669c6d6b5f8570e3a97a415057bea
David Robbins <robbins.david@gmail.com>
2022-05-26 13:48:34 +0000

    fixed typos

--- packages/accounts-admin-ui-bootstrap-3/package.js
+++ packages/accounts-admin-ui-bootstrap-3/package.js
@@ -4,28 +4,28 @@ Package.describe({
   name: "robbinsd:meteor-accounts-admin-ui-bootstrap-3"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.versionsFrom("METEOR@0.9.0");
 	api.use('standard-app-packages', ['client', 'server']);
 	api.use("alanning:roles", ['client', 'server']);
 
-	api.add_files('libs/user_query.js', ['client', 'server']);
+	api.addFiles('libs/user_query.js', ['client', 'server']);
 
-	api.add_files('client/startup.js', 'client');
-	api.add_files('client/accounts_admin.html', 'client');
-	api.add_files('client/accounts_admin.js', 'client');
-	api.add_files('client/delete_account_modal.html', 'client');
-	api.add_files('client/delete_account_modal.js', 'client');
-	api.add_files('client/info_account_modal.html', 'client');
-	api.add_files('client/info_account_modal.js', 'client');
-	api.add_files('client/update_account_modal.html', 'client');
-	api.add_files('client/update_account_modal.js', 'client');
-	api.add_files('client/update_roles_modal.html', 'client');
-	api.add_files('client/update_roles_modal.js', 'client');
+	api.addFiles('client/startup.js', 'client');
+	api.addFiles('client/accounts_admin.html', 'client');
+	api.addFiles('client/accounts_admin.js', 'client');
+	api.addFiles('client/delete_account_modal.html', 'client');
+	api.addFiles('client/delete_account_modal.js', 'client');
+	api.addFiles('client/info_account_modal.html', 'client');
+	api.addFiles('client/info_account_modal.js', 'client');
+	api.addFiles('client/update_account_modal.html', 'client');
+	api.addFiles('client/update_account_modal.js', 'client');
+	api.addFiles('client/update_roles_modal.html', 'client');
+	api.addFiles('client/update_roles_modal.js', 'client');
 
-	api.add_files('style/style.css', 'client');
+	api.addFiles('style/style.css', 'client');
 
-	api.add_files('server/startup.js', 'server');
-	api.add_files('server/publish.js', 'server');
-	api.add_files('server/methods.js', 'server');
+	api.addFiles('server/startup.js', 'server');
+	api.addFiles('server/publish.js', 'server');
+	api.addFiles('server/methods.js', 'server');
 });
